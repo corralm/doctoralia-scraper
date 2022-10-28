f0ad8d44cbcc534856cb89718059e9cbc94e8bae
Miguel Corral Jr <corraljrmiguel@gmail.com>
2022-10-28 20:58:47 -0300

    refactor: replace quotes per Xpath standards

--- doctoralia/doctoralia/spiders/doctoralia.py
+++ doctoralia/doctoralia/spiders/doctoralia.py
@@ -19,17 +19,17 @@ class Doctoralia(Spider):
 
         # Looks for the link to each specialization, builds a URL and yields a new
         # request to the pagination links.
-        spec_links = rc(".align-items-center .text-muted::attr(href)")
+        spec_links = rc('.align-items-center .text-muted::attr(href)')
         yield from rf(spec_links, self.parse)
 
        # Looks for the link to the next page, builds a URL and yields a new
        # request to the next page.
-        pagination_links = rx("//a[@aria-label='next']")
+        pagination_links = rx('//a[@aria-label="next"]')
         yield from rf(pagination_links, self.parse)
 
         # Follow all the links to each talk on the page calling the
         # parse_doctor callback for each of them.
-        dr_page_links = rx("//div[@class='media']/div[@class='pr-1']//@href")
+        dr_page_links = rx('//div[@class="media"]/div[@class="pr-1"]//@href')
         links = [l for l in dr_page_links.getall() if '/clinicas/' not in l]
         yield from rf(links, self.parse_doctor)
 
@@ -37,15 +37,15 @@ class Doctoralia(Spider):
         """Parses the response, extracting the scraped psychologist data as dicts."""
         rx = response.xpath
         # ZLApp.AppConfig
-        zr = rx("//script")[6]
+        zr = rx('//script')[6]
         # Google Tag Manager
-        gr = rx("//script")[8]
+        gr = rx('//script')[8]
 
         def parse_price(self, response):
             """Returns most common price from services provided."""
             # get numerical price list
-            sp = "//span[@data-id='service-price']"
-            pl = rx(f"//div[@class='media m-0']{sp}").re('\$\\xa0(.*)')
+            sp = '//span[@data-id="service-price"]'
+            pl = rx(f'//div[@class="media m-0"]{sp}').re("\$\\xa0(.*)")
             pg = (int(p.replace('.', '')) for p in pl)
             # get alternate price list
             vl = rx(f"{sp}/span/text()").getall()
@@ -66,11 +66,11 @@ class Doctoralia(Spider):
             'city2': gr.re_first("city'\]\s=\s'(.*?)'"),
             'region': gr.re_first("region'\]\s=\s'(.*?)'"),
             'specialization': gr.re_first("specialization'\]\s=\s'(.*?)'"),
-            'reviews': rx("//div/meta[@itemprop='reviewCount']/@content").get(),
-            # 'oldest_review_date': min(rx("//time/@datetime").getall(), default=''),
-            'newest_review_date': max(rx("//time/@datetime").getall(), default=''),
+            'reviews': rx('//div/meta[@itemprop="reviewCount"]/@content').get(),
+            # 'oldest_review_date': min(rx('//time/@datetime').getall(), default=''),
+            'newest_review_date': max(rx('//time/@datetime').getall(), default=''),
             'telemedicine': gr.re_first("virtual\-consultation\-profile'\]\s=\s'(.*?)'"),
             'price': parse_price(self, response),
             'url': gr.re_first("\['gtm\-url'\]\s=\s'(.*?)'"),
-            'fetch_time': f"{datetime.now():%Y-%m-%dT%H:%M:%S}",
+            'fetch_time': f'{datetime.now():%Y-%m-%dT%H:%M:%S}',
         }
